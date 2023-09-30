6426149cb9c19f6ca02d954710e8a920f05c5a68
IAmmarIftikhar <110023334+IAMMARKHAN@users.noreply.github.com>
2023-09-30 08:49:40 +0500

    Add Recover Passsword UI

--- Restuarant App/Restuarant App/RecoverPassword.Designer.cs
+++ Restuarant App/Restuarant App/RecoverPassword.Designer.cs
@@ -63,7 +63,7 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button2.Location = new System.Drawing.Point(42, 374);
+            this.button2.Location = new System.Drawing.Point(42, 368);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(97, 33);
             this.button2.TabIndex = 21;
@@ -79,7 +79,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(158, 374);
+            this.button1.Location = new System.Drawing.Point(158, 368);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 33);
             this.button1.TabIndex = 20;
