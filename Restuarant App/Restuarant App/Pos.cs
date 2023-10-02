83323a816bc453045f84d595dec76107a3fbf529
IAmmarIftikhar <110023334+IAMMARKHAN@users.noreply.github.com>
2023-10-02 12:49:56 +0500

    Add Pos Bill Module

--- Restuarant App/Restuarant App/Pos.cs
+++ Restuarant App/Restuarant App/Pos.cs
@@ -1,20 +1,33 @@
-﻿using db2021finalprojectg_9;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using db2021finalprojectg_9;
+using System.Diagnostics;
+
+using System.Windows.Forms;
+using iTextSharp.text;
 
 namespace Restuarant_App
 {
     public partial class Pos : Form
     {
         public string orderType="";
+        public string waiter = "";
+        public string delivery = "";
+        public int table;
+
+
+
         public Pos()
         {
             InitializeComponent();
@@ -39,13 +52,15 @@ namespace Restuarant_App
 
             try
             {
-                string query = "SELECT Id,Name,Category,Price,Size FROM menu";
+                string query = "SELECT Id, Name, CategoryId, Price, Size, Active FROM menu";
                 var con = Configuration.getInstance().getConnection();
                 SqlCommand command = new SqlCommand(query, con);
                 SqlDataAdapter adapter = new SqlDataAdapter(command);
                 DataTable dataTable = new DataTable();
                 adapter.Fill(dataTable);
                 dataGridView2.DataSource = dataTable;
+
+
             }
             catch (Exception ex)
             {
@@ -53,13 +68,14 @@ namespace Restuarant_App
             }
 
         }
+        
 
-        public void PopulateDataGridView1(string name)
+        public void PopulateDataGridView1(int name)
         {
             try
             {
                 var con = Configuration.getInstance().getConnection();
-                string query = "SELECT Id,Name,Category,Price,Size FROM menu Where Category=@a";
+                string query = "SELECT Id,Name,CategoryId,Price,Size, Active FROM menu Where CategoryId=@a";
                 SqlCommand command = new SqlCommand(query, con);
                 command.Parameters.AddWithValue("@a", name);
                 SqlDataAdapter adapter = new SqlDataAdapter(command);
@@ -102,6 +118,29 @@ namespace Restuarant_App
             }
         }
 
+        public string FindMethod1(int id)
+        {
+            try
+            {
+                var con = Configuration.getInstance().getConnection();
+                string query = "SELECT Name FROM dbo.[categories] WHERE Id=@a";
+                SqlCommand command = new SqlCommand(query, con);
+                command.Parameters.AddWithValue("@a", id);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    string name = reader["Name"].ToString();
+                    reader.Close();
+                    return name;
+                }
+                
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return "";
+        }
 
         private void LoadButtonsFromDatabase()
         {
@@ -141,19 +180,28 @@ namespace Restuarant_App
             // Add an event handler for the button click event, if needed
             // button.Click += YourButtonClickHandler;
             button.Click += Button_Click;
-
-
             // Add the button to TableLayoutPanel1
             tableLayoutPanel1.Controls.Add(button);
         }
         private void Button_Click(object sender, EventArgs e)
         {
-            // Cast the sender object back to a Button to get the clicked button
             Button clickedButton = (Button)sender;
-
-            // Now you can work with the clickedButton, for example:
             string buttonText = clickedButton.Text;
-            PopulateDataGridView1(buttonText);
+        
+                var con = Configuration.getInstance().getConnection();
+                string query = "SELECT Id FROM categories WHERE Name = @categoryName";
+                SqlCommand command = new SqlCommand(query, con);
+                command.Parameters.AddWithValue("@categoryName", buttonText);
+                object result = command.ExecuteScalar(); // Retrieve a single value (the ID)
+                if (result != null)
+                {
+                    int categoryId = Convert.ToInt32(result); // Convert the result to an integer
+                                                              // You can use the categoryId for further processing
+                    PopulateDataGridView1(categoryId);
+                }
+              
+            
+
            
         }
         private void tableLayoutPanel4_Paint(object sender, PaintEventArgs e)
@@ -188,15 +236,19 @@ namespace Restuarant_App
             {
                 int rowIndex = e.RowIndex; // Get the clicked row index
                 int idToDelete = Convert.ToInt32(dataGridView2.Rows[rowIndex].Cells["Id"].Value);
+                int category = Convert.ToInt32(dataGridView2.Rows[rowIndex].Cells["CategoryId"].Value);
                 DialogResult result = MessageBox.Show("Add This Item ? ", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                
                 if (result == DialogResult.Yes)
                 {
                     var itemData = FindMethod(idToDelete);
+                    var cat = FindMethod1(category);
+
                     if (itemData.Name != null)
                     {
                         string itemName = itemData.Name;
                         int itemPrice = itemData.Price;
-                        dataGridView1.Rows.Add(itemName,'1' ,itemPrice); // Assuming 1 as quantity
+                        dataGridView1.Rows.Add(itemName,cat,'1' ,itemPrice); // Assuming 1 as quantity
                         label5.Text = (int.Parse(label5.Text) + itemPrice).ToString();
                         decimal tax = (decimal.Parse(label5.Text) ) * 0.16m;
                         label7.Text = (int.Parse(label5.Text) + tax).ToString();
@@ -224,9 +276,10 @@ namespace Restuarant_App
             try
             {
                 var con = Configuration.getInstance().getConnection();
-                string query = "SELECT Id,Name,Category,Price,Size FROM menu Where Category=@a";
+                string query = "SELECT Id,Name,CategoryId,Price,Size FROM menu Where CategoryId=@a";
                 SqlCommand command = new SqlCommand(query, con);
                 command.Parameters.AddWithValue("@a", name);
+                //sql
                 SqlDataAdapter adapter = new SqlDataAdapter(command);
                 DataTable dataTable = new DataTable();
                 adapter.Fill(dataTable);
@@ -241,7 +294,6 @@ namespace Restuarant_App
 
         private void button2_Click(object sender, EventArgs e)
         {
-            orderType = "Dine In";
             button2.BackColor = Color.White;
             button2.ForeColor = Color.FromArgb(50, 55, 130);
 
@@ -251,13 +303,19 @@ namespace Restuarant_App
             button1.ForeColor = Color.White;
             button3.BackColor = Color.FromArgb(50, 55, 130); ;
             button3.ForeColor = Color.White;
+            DineInRequirements D = new DineInRequirements();
+            D.ShowDialog();
+            if(table!=0 && waiter!="")
+            {
+
+            orderType = "Dine In";
+            }    
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
 
-            orderType = "Delivery";
             button1.BackColor = Color.White;
             button1.ForeColor = Color.FromArgb(50, 55, 130);
 
@@ -267,7 +325,14 @@ namespace Restuarant_App
             button2.ForeColor = Color.White;
             button3.BackColor = Color.FromArgb(50, 55, 130); ;
             button3.ForeColor = Color.White;
+            DeliveryRequirements D = new DeliveryRequirements();
+            D.ShowDialog();
+            if(delivery!="")
+            {
+            orderType = "Delivery";
+            }
         }
+
 
         private void button3_Click(object sender, EventArgs e)
         {
@@ -281,21 +346,171 @@ namespace Restuarant_App
             button2.BackColor = Color.FromArgb(50, 55, 130); ;
             button2.ForeColor = Color.White;
         }
+        private void LogExceptionToDatabase(Exception ex)
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand command = new SqlCommand("INSERT INTO ErrorLog (ErrorMessage, StackTrace, FunctionName, FileName, LogTime) VALUES (@ErrorMessage, @StackTrace, @FunctionName, @FileName, @LogTime)", con);
+            command.Parameters.AddWithValue("@ErrorMessage", ex.Message);
+            command.Parameters.AddWithValue("@StackTrace", ex.StackTrace);
+            command.Parameters.AddWithValue("@FunctionName", GetCallingMethodName()); // Get calling method name
+            command.Parameters.AddWithValue("@FileName", GetFileName()); // Get file name
+            command.Parameters.AddWithValue("@LogTime", DateTime.Now);
 
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception logEx)
+            {
+                // Handle any exceptions that may occur during the logging operation (optional)
+                Console.WriteLine("Error while logging exception: " + logEx.Message);
+            }
+        }
+
+        // Helper function to extract calling method name from stack trace
+        private string GetCallingMethodName()
+        {
+            var frames = new StackTrace(true).GetFrames();
+            if (frames != null && frames.Length >= 3)
+            {
+                // Index 3 represents the calling method in the stack trace
+                return frames[3].GetMethod().Name;
+            }
+            return "Unknown";
+        }
+
+        // Helper function to extract file name from stack trace
+        private string GetFileName()
+        {
+            var frames = new StackTrace(true).GetFrames();
+            if (frames != null && frames.Length >= 3)
+            {
+                // Index 3 represents the calling method in the stack trace
+                var fileName = frames[3].GetFileName();
+                if (fileName != null)
+                {
+                    return System.IO.Path.GetFileName(fileName);
+                }
+            }
+            return "Unknown";
+        }
         private void button5_Click(object sender, EventArgs e)
         {
-            if(orderType=="")
+            bool c1=false, c2=false, c3=false;
+            if (orderType == "")
             {
                 MessageBox.Show("Please select the order type !");
             }
-            if (textBox1.Text == "")
+            else
             {
-                MessageBox.Show("Enter name of customer !");
+                c1= true;
             }
-            if (textBox1.Text == "Enter Customer Name")
+            if (textBox1.Text == "" || textBox1.Text == "Enter Customer Name")
             {
-                MessageBox.Show("Enter name of customer !");
+                MessageBox.Show("Enter valid customer name  !");
             }
+            else
+            {
+                c2=true;
+            }
+            if (dataGridView1.Rows.Count==0)
+            {
+                MessageBox.Show("Add any item first !");
+            }
+            else
+            {
+                c3 = true;
+            }
+            if (c1 == true && c2 == true && c3 == true)
+            {
+                DialogResult result = MessageBox.Show("Printing Bill ! Is Is Paid ?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+
+
+                // Check if the user clicked "Yes"
+                if (result == DialogResult.Yes)
+                {
+                    string savePath;
+
+                    // Show the Save File Dialog to get the file path
+                    using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                    {
+                        saveFileDialog.Filter = "PDF Files|*.pdf";
+                        try
+                        {
+                            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                            {
+                                savePath = saveFileDialog.FileName;
+
+                                // Get the desktop directory path
+                                Document document = new Document();
+
+                                // Create a PDF document
+                                PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(savePath, FileMode.Create));
+                                document.Open();
+
+                                // Add header information to the PDF
+                                Paragraph title = new Paragraph("Restaurant Management System");
+                                title.Alignment = Element.ALIGN_CENTER;
+                                document.Add(title);
+                                document.Add(new Paragraph("\n"));
+                                document.Add(new Paragraph($"Date: {DateTime.Today.ToShortDateString()}"));
+                                document.Add(new Paragraph($"Time: {DateTime.Now.ToShortTimeString()}"));
+                                document.Add(new Paragraph("Report Type: Bill"));
+                                document.Add(new Paragraph("Printed By: Manager"));
+
+                                document.Add(new Paragraph("\n"));
+
+                                // Create a table for the DataGridView content
+                                PdfPTable table = new PdfPTable(dataGridView1.Columns.Count);
+                                for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                                {
+                                    table.AddCell(new PdfPCell(new Phrase(dataGridView1.Columns[i].HeaderText))
+                                    {
+                                        HorizontalAlignment = Element.ALIGN_CENTER
+                                    });
+                                }
+
+                                // Add DataGridView content to the table
+                                foreach (DataGridViewRow row in dataGridView1.Rows)
+                                {
+                                    for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                                    {
+                                        table.AddCell(new PdfPCell(new Phrase(row.Cells[i].Value.ToString()))
+                                        {
+                                            HorizontalAlignment = Element.ALIGN_CENTER
+                                        });
+                                    }
+                                }
+
+                                // Add the table to the PDF document
+                                document.Add(table);
+                                // Add total, tax, and net total information to the PDF
+                                document.Add(new Paragraph("\n"));
+                                Paragraph P = new Paragraph($"Total: {label5.Text}");
+                                P.Alignment = Element.ALIGN_RIGHT;
+                                document.Add(P);
+                                Paragraph P1 = new Paragraph("Tax: 16%");
+                                P1.Alignment = Element.ALIGN_RIGHT;
+                                document.Add(P1);
+                                Paragraph P2 = new Paragraph($"Net Total: {label7.Text}");
+                                P2.Alignment = Element.ALIGN_RIGHT;
+                                document.Add(P2);
+                                document.Close();
+
+                                MessageBox.Show("Bill saved successfully !");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+
+                }
+            }
+
+
         }
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
@@ -308,6 +523,16 @@ namespace Restuarant_App
                     e.Handled = true; // Block the character if it's not an alphabet
                 }
             }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
