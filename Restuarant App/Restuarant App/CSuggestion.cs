2969efdc875d997e6a2a9dd1a0b9b74279e51963
IAmmarIftikhar <110023334+IAMMARKHAN@users.noreply.github.com>
2023-10-07 12:16:52 +0500

    Add Customer Suggestion Feature

--- Restuarant App/Restuarant App/CSuggestion.cs
+++ Restuarant App/Restuarant App/CSuggestion.cs
@@ -1,20 +1,57 @@
-﻿using System;
+﻿using db2021finalprojectg_9;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace Restuarant_App
 {
     public partial class CSuggestion : Form
     {
-        public CSuggestion()
+        public int cid;
+        public CSuggestion(int cid)
         {
             InitializeComponent();
+            this.cid = cid;
         }
+
+        private void CSuggestion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(richTextBox1.Text.Length > 0)
+            {
+                string query = "INSERT INTO suggestion (CustomerId, Suggesstion, CreatedAt, UpdatedAt) VALUES (@CustomerId, @Suggestion, @CreatedAt, @UpdatedAt);";
+                var con = Configuration.getInstance().getConnection();
+                SqlCommand command = new SqlCommand(query, con);
+                command.Parameters.AddWithValue("@CustomerId", cid); // replace with the name of your username input textbox
+                command.Parameters.AddWithValue("@Suggestion", richTextBox1.Text);
+                command.Parameters.AddWithValue("@CreatedAt", DateTime.Now); // replace with the name of your password input textbox
+                command.Parameters.AddWithValue("@UpdatedAt", DateTime.Now); // replace with the name of your password input textbox
+                command.ExecuteNonQuery();
+                MessageBox.Show("Thank You ! Your Suggestion Recorded !");
+
+            }
+            else
+            {
+
+            MessageBox.Show("Enter Suggestion First !");
+        }
+            }
     }
 }
