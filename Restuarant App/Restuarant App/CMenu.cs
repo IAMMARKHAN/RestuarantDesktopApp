4e16c8e95530298a94559235a257b5b236956c12
IAmmarIftikhar <110023334+IAMMARKHAN@users.noreply.github.com>
2023-10-13 10:54:22 +0500

    Add Customer Menu Logic

--- Restuarant App/Restuarant App/CMenu.cs
+++ Restuarant App/Restuarant App/CMenu.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using db2021finalprojectg_9;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,156 @@ namespace Restuarant_App
 {
     public partial class CMenu : Form
     {
+        public int Price;
+        public string Name, Desciption;
         public CMenu()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT Id, Name, CategoryId, Price, Size, Active FROM menu Where Active=@A";
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand command = new SqlCommand(query, con);
+            command.Parameters.AddWithValue("@A", true);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dataGridView2.DataSource = dataTable;
+        }
+        public void PopulateDataGridView1(int name)
+        {
+            try
+            {
+                var con = Configuration.getInstance().getConnection();
+                string query = "SELECT Id,Name,CategoryId,Price,Size, Active FROM menu Where CategoryId=@a And Active=@V";
+                SqlCommand command = new SqlCommand(query, con);
+                command.Parameters.AddWithValue("@a", name);
+                command.Parameters.AddWithValue("@V", true);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView2.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            string buttonText = "Desi";
+            var con = Configuration.getInstance().getConnection();
+            string query = "SELECT Id FROM categories WHERE Name = @categoryName";
+            SqlCommand command = new SqlCommand(query, con);
+            command.Parameters.AddWithValue("@categoryName", buttonText);
+            command.Parameters.AddWithValue("@B", true);
+            object result = command.ExecuteScalar(); // Retrieve a single value (the ID)
+            if (result != null)
+            {
+                int categoryId = Convert.ToInt32(result); // Convert the result to an integer
+                                                          // You can use the categoryId for further processing
+                PopulateDataGridView1(categoryId);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            string buttonText = "Italian";
+            var con = Configuration.getInstance().getConnection();
+            string query = "SELECT Id FROM categories WHERE Name = @categoryName";
+            SqlCommand command = new SqlCommand(query, con);
+            command.Parameters.AddWithValue("@categoryName", buttonText);
+            command.Parameters.AddWithValue("@B", true);
+            object result = command.ExecuteScalar(); // Retrieve a single value (the ID)
+            if (result != null)
+            {
+                int categoryId = Convert.ToInt32(result); // Convert the result to an integer
+                                                          // You can use the categoryId for further processing
+                PopulateDataGridView1(categoryId);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            string buttonText = "French";
+            var con = Configuration.getInstance().getConnection();
+            string query = "SELECT Id FROM categories WHERE Name = @categoryName";
+            SqlCommand command = new SqlCommand(query, con);
+            command.Parameters.AddWithValue("@categoryName", buttonText);
+            command.Parameters.AddWithValue("@B", true);
+            object result = command.ExecuteScalar(); // Retrieve a single value (the ID)
+            if (result != null)
+            {
+                int categoryId = Convert.ToInt32(result); // Convert the result to an integer
+                                                          // You can use the categoryId for further processing
+                PopulateDataGridView1(categoryId);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            string buttonText = "Spanish";
+            var con = Configuration.getInstance().getConnection();
+            string query = "SELECT Id FROM categories WHERE Name = @categoryName";
+            SqlCommand command = new SqlCommand(query, con);
+            command.Parameters.AddWithValue("@categoryName", buttonText);
+            command.Parameters.AddWithValue("@B", true);
+            object result = command.ExecuteScalar(); // Retrieve a single value (the ID)
+            if (result != null)
+            {
+                int categoryId = Convert.ToInt32(result); // Convert the result to an integer
+                                                          // You can use the categoryId for further processing
+                PopulateDataGridView1(categoryId);
+            }
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (e.ColumnIndex >= 0 && dataGridView2.Columns[e.ColumnIndex].Name == "Add")
+            {
+                DialogResult result = MessageBox.Show("Add This Item To Cart ?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                // Check if the user clicked "Yes"
+                if (result == DialogResult.Yes)
+                {
+                }
+
+                }
+        }
+
+        private void CMenu_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "SELECT Id, Name, CategoryId, Price, Size, Active FROM menu Where Active=@A";
+                var con = Configuration.getInstance().getConnection();
+                SqlCommand command = new SqlCommand(query, con);
+                command.Parameters.AddWithValue("@A", true);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView2.DataSource = dataTable;
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
     }
 }
