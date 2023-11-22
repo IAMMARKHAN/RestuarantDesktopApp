d9d8609ed24277170cce345ea29ecdaced83cf1a
IAmmarIftikhar <110023334+IAMMARKHAN@users.noreply.github.com>
2023-11-22 18:31:29 +0500

    Add Payment UI

--- Restuarant App/Restuarant App/Payment.cs
+++ Restuarant App/Restuarant App/Payment.cs
@@ -8,14 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using Stripe;
+
 
 namespace Restuarant_App
 {
     public partial class Payment : Form
     {
-        public Payment()
+        decimal amount;
+        public Payment(decimal A)
         {
-            InitializeComponent();
+            InitializeComponent();      
+            this.amount = A;
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -68,55 +72,76 @@ namespace Restuarant_App
 
         }
 
+        
         private void button1_Click(object sender, EventArgs e)
         {
-            if(textBox1.Text!="" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "")
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "")
             {
-                if(textBox1.Text.Length==16)
+                if (textBox1.Text.Length == 16)
                 {
                     if (textBox3.Text.Length == 3)
                     {
                         string input = textBox4.Text.Trim();
-
                         string pattern = @"^(0[1-9]|1[0-2])\/\d{2}$";
 
                         if (!Regex.IsMatch(input, pattern))
                         {
-                            MessageBox.Show("Invalid Date ! Enter In MM/YY Format !");
+                            MessageBox.Show("Invalid Date! Enter In MM/YY Format!");
                         }
                         else
                         {
-                            MessageBox.Show("Payment Successfull !");
-                            CCart C = new CCart("","");
-                            this.Hide();
-                            C.PaymentSuccessfull();
+                            StripeConfiguration.ApiKey = "sk_test_51OFCGyBv8EqRukFuQIL3mp6kR9uVw0xSGBz7Ntp9H2VTc0WP7pj3mUbwmngKLy4H7R3tRP1x2F2LXDhTZIW6STy500FvRDL4V4";
 
+                            var options = new TokenCreateOptions
+                            {
+                                Card = new TokenCardOptions
+                                {
+                                    Number = "tok_visa",
+                                    ExpMonth = "09",
+                                    ExpYear = "2028"
+                                }
+                            };
 
+                            var service = new TokenService();
+
+                            var chargeOptions = new ChargeCreateOptions
+                            {
+                                Amount = 1000,
+                                Currency = "usd",
+                                Description = "Payment For Food",
+                                Source = "tok_visa"
+                            };
+
+                            var chargeService = new ChargeService();
+                            Charge charge = chargeService.Create(chargeOptions);
+
+                            if (charge.Paid)
+                            {
+                                MessageBox.Show("Test Payment Successful!");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Test Payment Failed !");
+                            }
                         }
-
                     }
                     else
                     {
-                        MessageBox.Show("Enter Valid 3-Digit Cvc Number !");
+                        MessageBox.Show("Enter Valid 3-Digit CVC Number!");
                     }
-
                 }
                 else
                 {
-                    MessageBox.Show("Enter Valid 16-Digit Card Number !");
+                    MessageBox.Show("Enter Valid 16-Digit Card Number!");
                 }
-
             }
             else
             {
-                MessageBox.Show("Enter All Values !");
-
+                MessageBox.Show("Enter All Values!");
             }
         }
-
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            // Allow only numeric digits and control keys (e.g., Backspace)
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
             {
                 e.Handled = true;
@@ -125,10 +150,11 @@ namespace Restuarant_App
 
         private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar))
+            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && e.KeyChar != ' ')
             {
                 e.Handled = true;
             }
+
         }
 
         private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
